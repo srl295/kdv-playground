1b4eb64cd767c73a46fea4985ea2267f54ca724f
Steven R. Loomis <srl295@gmail.com>
2024-09-25 14:59:55 -0600

    cautiously add @keymanapp/common-types

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,29 +21,7 @@ export default function Home() {
         </ol>
 
         <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+          <p>Hello</p>
         </div>
       </main>
       <footer className={styles.footer}>
